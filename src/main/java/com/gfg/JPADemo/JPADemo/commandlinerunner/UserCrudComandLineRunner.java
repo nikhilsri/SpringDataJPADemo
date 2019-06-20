8b9256d1108e66c5059edc27ff48f08dfafc76c4
Nikhil <nikhil.srivastava944@gmail.com>
2019-06-20 22:55:40 +0530

    Added CRUD Operations using JPA Repository

--- src/main/java/com/gfg/JPADemo/JPADemo/commandlinerunner/UserCrudComandLineRunner.java
+++ src/main/java/com/gfg/JPADemo/JPADemo/commandlinerunner/UserCrudComandLineRunner.java
@@ -7,6 +7,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 import javax.jws.soap.SOAPBinding;
+import java.util.List;
 
 @Component
 public class UserCrudComandLineRunner implements CommandLineRunner {
@@ -21,6 +22,37 @@ public class UserCrudComandLineRunner implements CommandLineRunner {
         User user1=new User("Nikhil","Srivastava");
         userRepository.save(user1);
 
+        User user2=new User("Uncle","Bob");
+        userRepository.save(user2);
+
+        List<User> users=userRepository.findAll();
+
+        System.out.println("Before deletion "+users.toString());
+
+        userRepository.delete(user1);
+
+        users=userRepository.findAll();
+
+        System.out.println("After deletion "+users.toString());
+
+        user2.setFirstName("Jhonson");
+
+        userRepository.save(user2);
+
+        users=userRepository.findAll();
+
+        System.out.println("After Updation "+users.toString());
+
+        userRepository.save(new User("Smith","Rod"));
+        userRepository.save(new User("Uncle","Sam"));
+
+        users=userRepository.findByFirstName("Smith");
+
+        System.out.println("User with firstName Smith is "+users);
+
+        users=userRepository.findByFirstNameAndLastName("Uncle","Sam");
+
+        System.out.println("Finding by and "+users);
 
     }
 }
--- src/main/java/com/gfg/JPADemo/JPADemo/repository/UserRepository.java
+++ src/main/java/com/gfg/JPADemo/JPADemo/repository/UserRepository.java
@@ -4,8 +4,11 @@ import com.gfg.JPADemo.JPADemo.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+
 @Component
 public interface UserRepository  extends JpaRepository<User,Long> {
 
-
+    List<User> findByFirstName(String firstName);
+    List<User> findByFirstNameAndLastName(String firstName,String lastName);
 }
